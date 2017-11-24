e8f4478225612db15b65ee9a177df4e84012b179
Fritz Hsiao <fritz.hsiao@facebook.com>
2017-11-24 11:01:43 +0800

    Initial Commit

---
+++ calentest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calentest
+//
+//  Created by hsiao Wei lung on 2017/11/24.
+//  Copyright © 2017年 hsiao Wei lung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
